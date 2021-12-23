b963f01fb27e802cd765427b8f11713c5ae9c20a
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-23 15:24:42 +0900

    add protocols and delegates for CollectionView - connect collectionView with ViewController - add Collection View Delegate Methods for collectionView

--- 01-TheCardGame/TheCardGame/TheCardGame/ViewController.swift
+++ 01-TheCardGame/TheCardGame/TheCardGame/ViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     
     let model = CardModel()
     var cardsArray = [Card]()
@@ -17,8 +20,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         cardsArray = model.getCards()
+        
+        // Set the view controller as the datasource and delegate of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
+    
+    // MARK: - Collection View Delegate Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Return number of cards
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+        // Get a cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: Configure cell
+        
+        // Return cell
+        return cell
+    }
 
 }
 
