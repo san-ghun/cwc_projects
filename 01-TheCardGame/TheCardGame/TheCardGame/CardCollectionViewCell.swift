4e8f1953e4f2def6fe766ac693b9ea74454cf518
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-23 14:56:06 +0900

    add CardCollectionViewCell - add CardCollectionViewCell to manipulate collection view - connect CardModel with ViewController

---
+++ 01-TheCardGame/TheCardGame/TheCardGame/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  TheCardGame
+//
+//  Created by Sanghun Park on 2021/12/23.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+    
+}
