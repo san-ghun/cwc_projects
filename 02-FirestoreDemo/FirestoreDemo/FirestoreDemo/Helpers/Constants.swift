17f43b7a7539f6635c9dae009af360338960c87c
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-26 00:28:19 +0900

    Update FirestoreDemo - add Firebase SDK through Swift Package    - Firebase, FirebaseAuth, FirebaseAnalytics, Firestore - update ViewController    - style the buttons    - add video player to play video in the background - update SignUpViewController    - connect and style UI components    - add validateField() to validate text fields    - add IBAction signUpTapped() method to process sign up with FirebaseAuth - update LoginViewController    - connect and style UI components    - add IBAction loginTapped() method to process login with FirebaseAuth - add Utilities.swift to style UI components in ease and validate password with regular expression - add Constants.swift to manage Constants variable in Storyboard - add Shared.swift to manage functions that shared through ViewControllers

---
+++ 02-FirestoreDemo/FirestoreDemo/FirestoreDemo/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  FirestoreDemo
+//
+//  Created by Sanghun Park on 2021/12/25.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+}
--- 02-FirestoreDemo/FirestoreDemo/FirestoreDemo/ViewControllers/SignUpViewController.swift
+++ 02-FirestoreDemo/FirestoreDemo/FirestoreDemo/ViewControllers/SignUpViewController.swift
@@ -6,24 +6,110 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpElements() {
+        
+        // Hide the error label
+        errorLabel.alpha = 0
+        
+        // Style the elements
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(signUpButton)
     }
-    */
-
+    
+    // Check the fields and validate that the data is correct. If everything is correct, this method returns nil. Otherwise, it returns the error message
+    func validateField() -> String? {
+        
+        // Check that all fields are filled in
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        // Check if the password is secure
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            // Password isn't secure enough
+            return "Please make sure your password is at least 8 characters, contains a special character and a number."
+        }
+        
+        return nil
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+        
+        // Validate the fileds
+        let error = validateField()
+        
+        if error != nil {
+            
+            // There's something wrong with the fields, show error message
+            showError(errorLabel, error!)
+            
+        } else {
+            
+            // Create cleaned version of th data
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                
+                // Check for errors
+                if err != nil {
+                    
+                    // There was an error creating the user
+                    showError(self.errorLabel, "Error creating user")
+                    
+                } else {
+                    
+                    // User was created successfully, now store the first name and last name
+                    let db = Firestore.firestore()
+                    
+                    db.collection("users").addDocument(data: ["first_name": firstName, "last_name": lastName, "uid": result!.user.uid]) { (error) in
+                        
+                        if error != nil {
+                            // Show error message
+                            showError(self.errorLabel, "Error saving user data in server")
+                        }
+                    }
+                    
+                    // Transition to the home screen
+                    transitionToHome(self.storyboard, self.view)
+                }
+            }
+        }
+    }
 }
