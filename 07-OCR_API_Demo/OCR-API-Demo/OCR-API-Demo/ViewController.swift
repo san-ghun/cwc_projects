c4ef3532111da43cd75536371306f118b0f82606
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-28 21:29:04 +0900

    Init OCR-API-Demo - to practice API interaction with URL Requests

---
+++ 07-OCR_API_Demo/OCR-API-Demo/OCR-API-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCR-API-Demo
+//
+//  Created by Sanghun Park on 2021/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
