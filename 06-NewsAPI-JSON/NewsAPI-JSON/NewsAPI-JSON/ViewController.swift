25b48d482ab327e3f5270de9e8f437591f32ed29
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-28 21:22:12 +0900

    Update ViewController to get data and parse JSON - simple coding of using API and parsing JSON
    TODO: - Would be good to have more complex way to display articles with randomized API with some UI

--- 06-NewsAPI-JSON/NewsAPI-JSON/NewsAPI-JSON/ViewController.swift
+++ 06-NewsAPI-JSON/NewsAPI-JSON/NewsAPI-JSON/ViewController.swift
@@ -12,6 +12,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Hit the API endpoint
+        // API from NewsAPI, top headlines of business category in US
+        let urlString = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6919d675c7dc40b08279d861f38c6727"
+        
+        let url = URL(string: urlString)
+        
+        guard url != nil else { return }
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            // Check for errors
+            if error == nil && data != nil {
+                
+                // Parse JSON
+                let decoder = JSONDecoder()
+                
+                do {
+                    let newsFeed = try decoder.decode(NewsFeed.self, from: data!)
+                    
+                    print(newsFeed)
+                }
+                catch {
+                    print("Error in JSON parsing")
+                }
+                
+                
+            }
+        }
+        
+        // Make the API Call
+        dataTask.resume()
     }
 
 
