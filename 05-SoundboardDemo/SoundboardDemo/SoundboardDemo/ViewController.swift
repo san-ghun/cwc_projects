55d46bed9c6a8ecacb57719f3d107ecadf163af0
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-28 15:57:04 +0900

    Update storyboard and ViewController - add a Vertical Stack View    - add two Horizontal Stack Views in vertical stack view    - add two buttons to each horizontal stack views - connect buttons into IBAction in ViewController with nil

--- 05-SoundboardDemo/SoundboardDemo/SoundboardDemo/ViewController.swift
+++ 05-SoundboardDemo/SoundboardDemo/SoundboardDemo/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func aTapped(_ sender: Any) {
+    }
+    
+    @IBAction func bTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cTapped(_ sender: Any) {
+    }
+    
+    @IBAction func dTapped(_ sender: Any) {
+    }
+    
 }
 
