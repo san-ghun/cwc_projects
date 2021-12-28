90cdcf3da2a0b64bf99ee8bf15255ccf27160c14
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-28 16:18:44 +0900

    Update ViewController to play sound - make IBActions for each 4 buttons to play each sound file
    TODO: - Refactor IBActions that play sound for each button into one IBAction function for code reusability

--- 05-SoundboardDemo/SoundboardDemo/SoundboardDemo/ViewController.swift
+++ 05-SoundboardDemo/SoundboardDemo/SoundboardDemo/ViewController.swift
@@ -6,24 +6,89 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
     @IBAction func aTapped(_ sender: Any) {
+        
+        // Get the url
+        let url = Bundle.main.url(forResource: "a", withExtension: "mp3")
+        
+        // Make sure that have got the url, otherwias abord
+        guard url != nil else { return }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
     }
     
     @IBAction func bTapped(_ sender: Any) {
+        
+        // Get the url
+        let url = Bundle.main.url(forResource: "b", withExtension: "mp3")
+        
+        // Make sure that have got the url, otherwias abord
+        guard url != nil else { return }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
     }
     
     @IBAction func cTapped(_ sender: Any) {
+        
+        // Get the url
+        let url = Bundle.main.url(forResource: "c", withExtension: "mp3")
+        
+        // Make sure that have got the url, otherwias abord
+        guard url != nil else { return }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
     }
     
     @IBAction func dTapped(_ sender: Any) {
+        
+        // Get the url
+        let url = Bundle.main.url(forResource: "d", withExtension: "mp3")
+        
+        // Make sure that have got the url, otherwias abord
+        guard url != nil else { return }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
     }
     
 }
