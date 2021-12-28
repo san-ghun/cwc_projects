bc06776fb47f04f6d9a40f38676a560aa4bc048f
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-28 15:23:40 +0900

    Install Gemini with Cocoapods - install Gemini with pod - build UI with storyboard by adding CollectionView and ImageView - update ViewController with UICollectionView delegates    - add CollectionView with GeminiCollectionView - add MyCell.swift to call configured cell    - add MyCell class with GeminiCell
    ref) editing the storyboard to fit the image in simulator was quite annoyed. Look for CollectionView's Estimate Size as None in Storyboard.

--- 04-CollectionViewAnimation_GeminiDemo/GeminiDemo/GeminiDemo/ViewController.swift
+++ 04-CollectionViewAnimation_GeminiDemo/GeminiDemo/GeminiDemo/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import Gemini
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    let photos = ["1", "2", "3", "4", "5"]
+    
+    @IBOutlet weak var geminiCollectionView: GeminiCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        geminiCollectionView.dataSource = self
+        geminiCollectionView.delegate = self
+        
+        // Configure the animation
+        geminiCollectionView.gemini
+            .rollRotationAnimation()
+            .degree(45)
+            .rollEffect(.rollUp)
+        
     }
 
+    
+    // MARK: - UICollectionViewDataSource
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gemCell", for: indexPath) as! MyCell
+        
+        cell.setCell(imageName: photos[indexPath.row])
+        
+        // Animate
+        geminiCollectionView.animateCell(cell)
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        // Animate
+        geminiCollectionView.animateVisibleCells()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        // Animate
+        if let cell = cell as? MyCell {
+            geminiCollectionView.animateCell(cell)
+        }
+    }
 }
 
