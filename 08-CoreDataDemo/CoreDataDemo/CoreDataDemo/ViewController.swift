65617e26668dee8ee9125e6ddbbadb4da2a5d68a
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-29 17:46:28 +0900

    Init CoreDataDemo - to practice CoreData

---
+++ 08-CoreDataDemo/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Sanghun Park on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
