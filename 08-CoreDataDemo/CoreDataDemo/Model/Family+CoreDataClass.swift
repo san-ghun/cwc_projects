da737e75b87cc261c440f5fd9661e9c7efd2135c
Sanghun <po4865+github_sanghun@gmail.com>
2021-12-29 22:50:18 +0900

    Add new entity and relationship in CoreData - Add Family entity - Add relationship between Family and Person entities

---
+++ 08-CoreDataDemo/CoreDataDemo/Model/Family+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Family+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Sanghun Park on 2021/12/29.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Family)
+public class Family: NSManagedObject {
+
+}
---
+++ 08-CoreDataDemo/CoreDataDemo/Model/Person+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Person+CoreDataProperties.swift
+//  CoreDataDemo
+//
+//  Created by Sanghun Park on 2021/12/29.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Person {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Person> {
+        return NSFetchRequest<Person>(entityName: "Person")
+    }
+
+    @NSManaged public var age: Int64
+    @NSManaged public var gender: String?
+    @NSManaged public var name: String?
+    @NSManaged public var family: Family?
+
+}
+
+extension Person : Identifiable {
+
+}
