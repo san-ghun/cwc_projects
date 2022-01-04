e7a2a8ae74ff503b6007cb7a155f181fc686d82e
Sanghun <po4865+github_sanghun@gmail.com>
2022-01-04 16:11:59 +0900

    Add storyboard UI for demo - Add Navigation Controller to control views - Add TableView to display table of data - Add View to create, edit and delete data - Update ViewController.swift code for basic template for TableView control

--- 09-MongodbNoteDemo/MongodbNoteDemo/MongodbNoteDemo/ViewController.swift
+++ 09-MongodbNoteDemo/MongodbNoteDemo/MongodbNoteDemo/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var notesArray = ["Note1", "Note2", "Note3", "Note4", "Note5"]
 
+    @IBOutlet weak var notesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        notesTableView.delegate = self
+        notesTableView.dataSource = self
+        
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "prototypeCell", for: indexPath)
+        
+        let note = self.notesArray[indexPath.row]
+        
+//        cell.textLabel?.text = notesArray[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = note
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 }
 
