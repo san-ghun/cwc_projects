20a90ba8bc9804c73905615efb3452c20411e3e9
Sanghun <po4865@gmail.com>
2022-04-02 17:40:49 +0900

    init: Initiate demo project to practice UIPanGesture

---
+++ 12-UIPanGesture-Demo/UIPanGesture-Demo/UIPanGesture-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPanGesture-Demo
+//
+//  Created by Sanghun Park on 2022/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
