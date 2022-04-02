11f2d372fac23ed290d6e2dc5fe55111577a6115
Sanghun <po4865@gmail.com>
2022-04-02 20:45:33 +0900

    feat: Make basic UI prior to implement interaction
    - Add UI components in Main.storyboard
    - Add comments for marking and connect IBOutlets

--- 12-UIPanGesture-Demo/UIPanGesture-Demo/UIPanGesture-Demo/ViewController.swift
+++ 12-UIPanGesture-Demo/UIPanGesture-Demo/UIPanGesture-Demo/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var fileImageView: UIImageView!
+    @IBOutlet weak var trashImageView: UIImageView!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    // MARK: - Methods
+    
+    // MARK: Custom Methods
+    
+    // MARK: IBActions
+    
 }
 
